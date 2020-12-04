9aa873d6db81552433b80f38625247d2d38b903f
DanielSchroeder <DanielSchroeder@0-208-14>
2020-12-04 11:03:24 +0100

    Vertex fertig S.54

--- Zocken2020/src/Test.java
+++ Zocken2020/src/Test.java
@@ -3,17 +3,19 @@ public class Test {
 
 	public static void main(String[] args) {
 		Vertex v1 = new Vertex(17, 4);
-		Vertex v2 = new Vertex(42, 0);
-		Vertex vv = new Vertex(0, 0);
+		Vertex v2 = new Vertex(42, 23);
+		Vertex v3 = new Vertex(0, 0);
+		Vertex v4 = new Vertex(42, 23);
 
-		System.out.println("v1 ="+ v1);
+		System.out.println("v1 =" + v1);
 		System.out.println(v2.toString());
 
 		v1.y = 2 * v1.y;
 
 		System.out.println(v1.toString());
-		String s1= "hallo";
-		String s2="Hallo". toLowerCase ();
+		String s1 = "hallo";
+		String s2 = "Hallo".toLowerCase();
 		System.out.println(s1.equals(s2));
-		
-}}
+
+	}
+}
