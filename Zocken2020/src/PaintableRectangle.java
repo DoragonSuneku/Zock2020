01fbd1bd745c2defd9835e40949bbeb6fc717305
losch <losch@192.168.0.80>
2021-01-31 23:41:37 +0100

    A19 Paintable Rect

---
+++ Zocken2020/src/PaintableRectangle.java
@@ -0,0 +1,9 @@
+public class PaintableRectangle extends SimpleOval implements Paintable {
+
+	public PaintableRectangle(double width, double height, double x, double y) {
+		super(width, height, x, y);
+	}
+
+	public void paintTo(java.awt.Graphics g) {
+		g.fillRect((int)pos.x, (int) pos.y, (int) width, (int) height);	}
+}
